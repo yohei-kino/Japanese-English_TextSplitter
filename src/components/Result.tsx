9b4d18014f164cb4f755269133f6c63ab336a6d1
Kakiuchi Yohei <nedaruna0201@gmail.com>
2024-09-19 23:03:39 +0900

    PCビュー完成

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -6,6 +6,11 @@ interface ResultProps {
 }
 
 export default function Result({ output }: ResultProps) {
+  // outputが空の場合は何も表示しない
+  if (!output.japaneseText && !output.englishText) {
+    return null;
+  }
+
   // 日本語の文字数をカウント
   const japaneseCharacterCount = output.japaneseText.length;
 
@@ -16,14 +21,26 @@ export default function Result({ output }: ResultProps) {
     .filter((word) => word.length > 0).length;
 
   return (
-    <div>
-      <h2>日本語のテキスト:</h2>
-      <p>{output.japaneseText}</p>
-      <p>文字数: {japaneseCharacterCount}</p>
+    <div className="mx-auto block w-3/4 p-[8px] mt-[24px] text-[18px]">
+      <div className="flex gap-[8px] justify-center mb-[8px]">
+        <img src="./jp_flag.webp" alt="japan flag" width={25} height={25} />
+        <h2>日本語のテキスト:</h2>
+        <p>文字数 {japaneseCharacterCount}</p>
+      </div>
+      <div className="border-[1px] border-[#000] border-solid text-[15px] bg-blue-50">
+        <p className="p-[2px]">{output.japaneseText}</p>
+      </div>
 
-      <h2>英語のテキスト:</h2>
-      <p>{output.englishText}</p>
-      <p>単語数: {englishWordCount}</p>
+      <div className="mt-[24px]">
+        <div className="flex gap-[8px] justify-center mb-[8px]">
+          <img src="./uni_flag.webp" alt="japan flag" width={25} height={25} />
+          <h2>英語のテキスト:</h2>
+          <p>単語数 {englishWordCount}</p>
+        </div>
+        <div className="border-[1px] border-[#000] border-solid text-[15px] bg-red-50">
+          <p className="p-[2px]">{output.englishText}</p>
+        </div>
+      </div>
     </div>
   );
 }
